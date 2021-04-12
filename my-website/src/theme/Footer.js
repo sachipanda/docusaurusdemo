cf60b86527cd7aefb16f1c755216fee99cbe7438
DeanGodfree <deangodfree@gmail.com>
2021-04-12 17:15:25 +0100

    Update Footer.js

--- my-website/src/theme/Footer.js
+++ my-website/src/theme/Footer.js
@@ -5,7 +5,8 @@ import React from 'react';
 export default function Footer(props) {
   return (
     <>
-      <div>Before footer</div>
+      <script async defer src="https://teams.microsoft.com/share/launcher.js"></script>
+      <div>Share this page in MS Teams <a class="teams-share-button" data-href="https://sachipanda.github.io/docusaurusdemo/docs/create-a-blog-post">Share this link</a></div>
       <OriginalFooter {...props} />
     </>
   );
