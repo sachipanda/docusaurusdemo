05ab20bb8d2e737ac0b93f3d66b3e1f97cab25b6
DeanGodfree <deangodfree@gmail.com>
2021-04-12 20:31:41 +0100

    Update Footer.js

--- my-website/src/theme/Footer.js
+++ my-website/src/theme/Footer.js
@@ -2,8 +2,6 @@
 import OriginalFooter from '@theme-original/Footer';
 import React from 'react';
 import {useLocation} from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-
 
 export default function Footer(props) {
 const location = useLocation();
@@ -14,7 +12,7 @@ console.log(useBaseUrl(location.pathname));
   return (
     <>
       <script async defer src="https://teams.microsoft.com/share/launcher.js"></script>
-      <div>Share this page in MS Teams <a class="teams-share-button" data-href="https://sachipanda.github.io"+ { location.pathname }>Share this link</a></div>
+      <div>Share this page in MS Teams <a class="teams-share-button" data-href="https://sachipanda.github.io" { location.pathname }>Share this link</a></div>
       <OriginalFooter {...props} />
     </>
   );
